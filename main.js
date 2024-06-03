dc5887ad8765ce3a60ea6682d52b0be6c082242d
ihell269@gmail.com <ihell269@gmail.com>
2024-06-03 10:42:10 +0700

    tambah javascript dan komentar

---
+++ main.js
@@ -0,0 +1,55 @@
+//stemen module firebase 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+// konfigurasi firebase 
+const firebaseConfig = {
+  apiKey: "AIzaSyB8g6kCR8laDnH1YCF9cYVy10lF1y7s2i0",
+  authDomain: "bakwan-jagung.firebaseapp.com",
+  projectId: "bakwan-jagung",
+  storageBucket: "bakwan-jagung.appspot.com",
+  messagingSenderId: "710653450064",
+  appId: "1:710653450064:web:5cbf9ff4240922b03ffe55",
+  measurementId: "G-VBHD7G08PJ"
+};
+
+// Inisialisasi firebase 
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//fungsi untuk mengambil daftar produk dari firebase
+export async function ambilDaftarPembeli() {
+  const refDokumen = collection(db, "pembeli");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      nama: dok.data().nama,
+      alamat: dok.data().alamat,
+      noTlpn: dok.data().noTlpn,
+    });
+  });
+
+
+
+  return hasil;
+}
+
+//fungsi untuk memformat angka dan memisahkan ribuan pakai titik
+export function formatAngka(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
